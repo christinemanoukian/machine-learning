9d35189e03d565d7f076b5817a9be1867c72a9d5
christine318 <89437543+christinemanoukian@users.noreply.github.com>
2021-09-19 04:05:13 +0000

    Fixed tests for Assignment Set 3.

--- src/matrix.py
+++ src/matrix.py
@@ -38,12 +38,39 @@ class Matrix:
 
     def matrix_multiply(self, new_matrix):
         if self.num_cols != new_matrix.num_rows:
-            print('cannot multiply matrices')
+            return 'cannot multiply matrices'
         else:
             result = [[0 for x in range(new_matrix.num_cols)] for y in range(self.num_rows)]
             for i in range(len(result)):
                 for j in range(len(result[i])):
                     for k in range(self.num_cols):
                         result[i][j] += self.elements[i][k] * new_matrix.elements[k][j]
+            final = []
             for lst in result:
-                print(lst)
+                final.append(lst)
+            return Matrix(final)
+
+    def get_smaller_matrix(self, i):
+        if self.num_cols == 2 and self.num_rows == 2:
+            return self.elements
+        else:
+            result = []
+            for lst in self.elements:
+                if self.elements.index(lst) != 0:
+                    copy_lst = list(lst)
+                    copy_lst.remove(lst[i])
+                    result.append(copy_lst)
+            return result
+
+    def calc_determinant(self):
+        if self.num_cols != self.num_rows:
+            return 'cannot find determinant'
+        if self.num_cols == 2 and self.num_rows == 2:
+            result = self.elements[0][0] * self.elements[1][1] - self.elements[0][1] * self.elements[1][0]
+        else:
+            result = 0
+            for i in range(len(self.elements[0])):
+                item = self.elements[0][i]
+                smaller_matrix = Matrix(self.get_smaller_matrix(i))
+                result += (-1) ** i * item * smaller_matrix.calc_determinant()
+        return result
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -2,39 +2,96 @@ import sys
 sys.path.append('src')
 from matrix import Matrix
 
-print('test1')
 A = Matrix([[1,2], [3,4]])
-A.print()
-At = A.transpose()
-At.print()
-A.add(At).print()
 other_matrix = Matrix([[1,1], [1,1]]) 
-A.add(other_matrix).print() # should print [[2,3], [4,5]]
-A.subtract(other_matrix).print() # should print [[0,1], [2,3]]
-A.scalar_multiply(2).print() # should print [[2,4], [6,8]]
 other_matrix_2 = Matrix([[2,2], [2,2]]) 
-A.matrix_multiply(other_matrix_2) # should print [[6,6], [14,14]]
+At = A.transpose()
+Aat = A.add(At)
+Aa = A.add(other_matrix)
+As = A.subtract(other_matrix)
+Asm = A.scalar_multiply(2)
+Am = A.matrix_multiply(other_matrix_2)
+if At.elements != [[1,3], [2,4]]:
+    print('Actual matrix does not equal intended matrix') 
+if Aat.elements != [[2,5], [5,8]]:
+    print('Actual matrix does not equal intended matrix') 
+if Aa.elements != [[2,3], [4,5]]:
+    print('Actual matrix does not equal intended matrix')
+if As.elements != [[0,1], [2,3]]:
+    print('Actual matrix does not equal intended matrix')
+if Asm.elements != [[2,4], [6,8]]:
+    print('Actual matrix does not equal intended matrix')
+if Am.elements != [[6,6],[14,14]]:
+    print('Actual matrix does not equal intended matrix')
 
-print('test2')
 B = Matrix([[1,2,3], [4,5,6]])
-B.print()
-Bt = B.transpose()
-Bt.print()
 other_matrix = Matrix([[1,1,1], [1,1,1]])
-B.add(other_matrix).print() # should print [[2,3,4], [5,6,7]]
-B.subtract(other_matrix).print() # should print [[0,1,2], [3,4,5]]
-B.scalar_multiply(2).print() # should print [[2,4,6], [8,10,12]]
 other_matrix_2 = Matrix([[2,2,2], [2,2,2]])
-B.matrix_multiply(other_matrix_2) # should print 'cannot multiply matrices'
+Bt = B.transpose()
+Ba = B.add(other_matrix)
+Bs = B.subtract(other_matrix)
+Bsm = B.scalar_multiply(2)
+if Bt.elements != [[1,4], [2,5], [3,6]]:
+    print('Actual matrix does not equal intended matrix')
+if Ba.elements != [[2,3,4], [5,6,7]]:
+    print('Actual matrix does not equal intended matrix')
+if Bs.elements != [[0,1,2], [3,4,5]]:
+    print('Actual matrix does not equal intended matrix')
+if Bsm.elements != [[2,4,6], [8,10,12]]:
+    print('Actual matrix does not equal intended matrix')
+if B.matrix_multiply(other_matrix_2) != 'cannot multiply matrices':
+    print('failed')
 
-print('test3')
 C = Matrix([[1,2], [3,4], [5,6]])
-C.print()
-Ct = C.transpose()
-Ct.print()
 other_matrix = Matrix([[1,1], [1,1], [1,1]])
-C.add(other_matrix).print() # should print [[2,3], [4,5], [6,7]]
-C.subtract(other_matrix).print() # should print [[0,1], [2,3], [4,5]]
-C.scalar_multiply(2).print() # should print [[2,4], [6,8], [10,12]]
 other_matrix_2 = Matrix([[1,2,3,4], [3,4,1,5]])
-C.matrix_multiply(other_matrix_2) # should print [[7,10,5,14], [15,22,13,32], [23,34,21,50]]
+Ct = C.transpose()
+Ca = C.add(other_matrix)
+Cs = C.subtract(other_matrix)
+Csm = C.scalar_multiply(2)
+Cm = C.matrix_multiply(other_matrix_2)
+if Ct.elements != [[1,3,5], [2,4,6]]:
+    print('Actual matrix does not equal intended matrix')
+if Ca.elements != [[2,3], [4,5], [6,7]]:
+    print('Actual matrix does not equal intended matrix')
+if Cs.elements != [[0,1], [2,3], [4,5]]:
+    print('Actual matrix does not equal intended matrix')
+if Csm.elements != [[2,4], [6,8], [10,12]]:
+    print('Actual matrix does not equal intended matrix')
+if Cm.elements != [[7,10,5,14], [15,22,13,32], [23,34,21,50]]:
+    print('Actual matrix does not equal intended matrix')
+
+D = Matrix([[1,2,3], [4,5,6], [7,8,9]])
+other_matrix = Matrix([[0,1,2], [3,4,5], [6,7,8]])
+other_matrix_2 = Matrix([[2,3,4], [5,6,7], [8,9,0]])
+Dt = D.transpose()
+Da = D.add(other_matrix)
+Ds = D.subtract(other_matrix)
+Dsm = D.scalar_multiply(9)
+Dm = D.matrix_multiply(other_matrix_2)
+if Dt.elements != [[1,4,7], [2,5,8], [3,6,9]]:
+    print('Actual matrix does not equal intended matrix')
+if Da.elements != [[1,3,5], [7,9,11], [13,15,17]]:
+    print('Actual matrix does not equal intended matrix')
+if Ds.elements != [[1,1,1], [1,1,1], [1,1,1]]:
+    print('Actual matrix does not equal intended matrix')
+if Dsm.elements != [[9,18,27], [36,45,54], [63,72,81]]:
+    print('Actual matrix does not equal intended matrix')
+if Dm.elements != [[36,42,18], [81,96,51], [126,150,84]]:
+    print('Actual matrix does not equal intended matrix')
+
+E = Matrix([[5,5,3], [1,9,6], [6,8,8]])
+if E.calc_determinant() != 122:
+    print('failed to calculate determinant')
+
+F = Matrix([[2,5,2,2], [6,7,6,6], [2,5,5,5], [3,8,8,3]])
+if F.calc_determinant() != 1030:
+    print('failed to calculate determinant')
+
+G = Matrix([[3,1], [6,2]])
+if G.calc_determinant() != 0:
+    print('failed to calculate determinant')
+
+H = Matrix([[1,2], [4,3], [4,1]])
+if H.calc_determinant() != 'cannot find determinant':
+    print('failed to calculate determinant')
