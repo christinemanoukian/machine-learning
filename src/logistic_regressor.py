b9e178595429c1af8e7bd478c53920048294efed
christinemanoukian <christinemanoukian18@gmail.com>
2021-11-15 04:58:43 +0000

    Finished Set 11

--- src/logistic_regressor.py
+++ src/logistic_regressor.py
@@ -6,16 +6,18 @@ from math import exp as e
 
 
 class LogisticRegressor:
-    def __init__(self):
+    def __init__(self, m, M):
         self.coefficients = None
+        self.m = m
+        self.M = M
 
-    def fit(self, data):
+    def fit(self, data, interaction_terms):
         y = []
         for point in data:
             value = []
             count = len(point) - 1
             if count > 0:
-                value.append(ln((1 / point[count]) - 1))
+                value.append(ln((self.M - point[count]) / (point[count] - self.m)))
                 count -= 1
             y.append(value)
         y = Matrix(y)
@@ -24,29 +26,66 @@ class LogisticRegressor:
             x = []
             count = 0
             while count < len(point) - 1:
-                x.append(point[count])
+                x.append(point[count])                    
                 count += 1
-            x.append(1)
-            X.append(x)
+            if interaction_terms == True:
+                count1 = 0
+                count2 = 1
+                while count1 <= len(point) - 3 and count2 <= len(point) - 1:
+                    if count2 == len(point) - 1:
+                        count2 = 1
+                        count1 += 1
+                    if count1 >= count2:
+                        count2 += 1
+                    else:
+                        x.append(point[count1] * point[count2])
+                        count2 += 1
+            x.append(1)                
+            X.append(x) 
+        X = [[round(num, 1) for num in lst] for lst in X]
         X = Matrix(X)
         Xt = X.transpose()
         XtX = Xt.matrix_multiply(X)
+        XtX = [[round(num, 2) for num in lst] for lst in XtX.elements]
+        XtX = Matrix(XtX)
         Xty = Xt.matrix_multiply(y)
         XtX_inverse = XtX.inverse()
         result = XtX_inverse.matrix_multiply(Xty)
         final = []
         for lst in result.elements:
             final.append(lst[0])
+        final = [round(num, 2) for num in final]
         self.coefficients = final
 
     def predict(self, lst):
         length = len(self.coefficients) - 1
         e_exponent = self.coefficients[length]
-        count = 0
-        while count < length:
-            e_exponent += self.coefficients[count] * lst[count]
-            count += 1
+        count1 = 0
+        count2 = 0
+        while count2 < len(lst):
+            e_exponent += self.coefficients[count1] * lst[count2]
+            count1 += 1
+            count2 += 1
+        if interaction_terms == True:
+            count1 = 0
+            count2 = 1
+            while count1 <= len(lst) - 3 and count2 <= len(lst) - 1:
+                if count2 == len(lst) - 1:
+                    count2 = 1
+                    count1 += 1
+                if count1 >= count2:
+                    count2 += 1
+                else:
+                    e_exponent += lst[count1] * lst[count2]
+                    count2 += 1            
         e_value = e(e_exponent)
         denominator = 1 + e_value
-        result = 1 / denominator
+        numerator = self.M - self.m
+        result = self.m + numerator / denominator
+        result = round(result, 1)
         return result
+
+reg = LogisticRegressor(0, 10)
+data = [[0,0,1], [1,0,2], [2,0,4], [4,0,8], [6,0,9], [0,2,2], [0,4,5], [0,6,7], [0,8,6], [2,2,1], [3,4,1]]
+interaction_terms = True
+reg.fit(data, interaction_terms)
---
+++ tests/test_logistic.py
@@ -0,0 +1,16 @@
+# I made another test file for the logistic regression because for the last one, none of them had bounds and so it would throw off all the data / answers
+
+import sys
+sys.path.append('src')
+from logistic_regressor import LogisticRegressor
+
+reg = LogisticRegressor(0, 10)
+data = [[0,0,1], [1,0,2], [2,0,4], [4,0,8], [6,0,9], [0,2,2], [0,4,5], [0,6,7], [0,8,6], [2,2,1], [3,4,1]]
+interaction_terms = True
+reg.fit(data, interaction_terms)
+if reg.coefficients != [-.74, -.38, .35, 2.06]:
+    print('reg.coefficients failed on input [[0,0,1], [1,0,2], [2,0,4], [4,0,8], [6,0,9], [0,2,2], [0,4,5], [0,6,7], [0,8,6], [2,2,1], [3,4,1]]')
+if reg.predict([5,5]) != 9.7:
+    print('reg.predict failed on input [5,5]')
+if reg.predict([5,0]) != 8.4:
+    print('reg.predict failed on input [5,0]')
